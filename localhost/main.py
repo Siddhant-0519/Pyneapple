5f40a88b9c1946953511ca226ef9be524804a2af
Siddhant Purohit <siddhant.0519@gmail.com>
2023-08-17 13:42:15 -0700

    Added weights dropdown, slider finetune, filehosting endpoint, labels output consistent

--- localhost/main.py
+++ localhost/main.py
@@ -8,6 +8,7 @@ from fastapi import FastAPI, File, UploadFile
 from typing import List
 import numpy as np
 from Pyneapple.pyneapple.regionalization.expressive_maxp import expressive_maxp
+from Pyneapple.pyneapple.regionalization.maxp import maxp
 import geopandas as gpd
 import uvicorn
 from read_shapefile import read_shapefile
@@ -19,10 +20,32 @@ from Pyneapple.pyneapple.regionalization.generalized_p import generalized_p
 import time
 from spopt.region.maxp import maxp as libMaxP
 from Pyneapple.pyneapple.regionalization.scalable_maxp import scalable_maxp
+from libpysal.weights import Queen, Rook
+from fastapi.middleware.cors import CORSMiddleware
+import json
+
 app = FastAPI()
 
-data_dir = "D:/user_pa1n/VSCode/projects/pyneapple_v2/testData"
-weights_dir = "D:/user_pa1n/VSCode/projects/pyneapple_v2/Pyneapple/pyneapple/weight"
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Allows CORS from this origin
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all HTTP methods
+    allow_headers=["*"],
+)
+
+data_dir = "../testData"
+# weights_dir = "D:/user_pa1n/VSCode/projects/pyneapple_v2/Pyneapple/pyneapple/weight"
+
+
+@app.get("/files/{filename}")
+async def read_file_fe(filename: str):
+    gdf = read_shapefile(data_dir, filename)
+    gdf = gdf[['geometry']]
+    gdf['id'] = range(1, len(gdf) + 1)
+    # print(gdf)
+    return json.loads(gdf.to_json())
 
 
 @app.post("/upload")
@@ -46,16 +69,24 @@ async def list_files():
     return files
 
 
+@app.get("/dfDetails")
+async def df_details(filename):
+    
+    df = read_shapefile(data_dir, filename)
+    numeric_cols = df.select_dtypes(include=['number']).columns
+    available_cols = [(col, df[col].min().item(), df[col].max().item()) for col in numeric_cols]
+    print("Available_cols: ", available_cols)
+    return available_cols
+
+
 @app.get("/listWeights")
 async def list_weights():
-    weights = []
-    for weight in os.listdir(weights_dir):
-        weights.append(weight)
-    return weights   
+    weights = ['Queen', 'Rook']
+    return weights 
 
 
 @app.get("/api/endpoint/emp")
-def empEndPoint(file_name: str, disname: str,
+def empEndPoint(file_name: str, weight: str, disname: str,
                 minName: str, minLow: float, minHigh: float,
                 maxName: str, maxLow: float, maxHigh: float,
                 avgName: str, avgLow: float, avgHigh: float,
@@ -63,7 +94,12 @@ def empEndPoint(file_name: str, disname: str,
                 countLow: float, countHigh: float):
 
     df = read_shapefile(data_dir, file_name)
-    w = rook(df)
+    if weight == 'Rook':
+        w = Rook.from_dataframe(df)
+    elif weight == 'Queen':
+        w = Queen.from_dataframe(df)
+    else:
+        print("Invalid weight selected")
 
     empStartTime = time.time()
     max_p, labels = expressive_maxp(df, w, disname, minName, minLow, minHigh,
@@ -78,11 +114,16 @@ def empEndPoint(file_name: str, disname: str,
 
 
 @app.get("/api/endpoint/generalizedP")
-def gmpEndPoint(file_name: str, sim_attr: str, ext_attr: str,
+def gmpEndPoint(file_name: str, weight: str, sim_attr: str, ext_attr: str,
                 threshold: float, p: int):
 
     df = read_shapefile(data_dir, file_name)
-    w = rook(df)
+    if weight == 'Rook':
+        w = Rook.from_dataframe(df)
+    elif weight == 'Queen':
+        w = Queen.from_dataframe(df)
+    else:
+        print("Invalid weight selected")
     prucStartTime = time.time()
     prucLabels = generalized_p(df, w, sim_attr, ext_attr, threshold, p)
     prucEndTime = time.time()
@@ -93,10 +134,15 @@ def gmpEndPoint(file_name: str, sim_attr: str, ext_attr: str,
 
 
 @app.get("/api/endpoint/libraryMaxP")
-def libraryMaxP(file_name: str, attr_name: str, threshold_name: str, threshold: float):
+def libraryMaxP(file_name: str, weight: str, attr_name: str, threshold_name: str, threshold: float):
 
     df = read_shapefile(data_dir, file_name)
-    w = rook(df)
+    if weight == 'Rook':
+        w = Rook.from_dataframe(df)
+    elif weight == 'Queen':
+        w = Queen.from_dataframe(df)
+    else:
+        print("Invalid weight selected")
     lmpStartTime = time.time()
     lmpLabels = libMaxP(df, w, attr_name, threshold_name, threshold, 2)
     lmpEndTime = time.time()
@@ -107,10 +153,16 @@ def libraryMaxP(file_name: str, attr_name: str, threshold_name: str, threshold: 
 
 
 @app.get("/api/endpoint/ScalableMaxP")
-def scalable_p(file_name: str, sim_attr: str, ext_attr: str, threshold: float):
+def smp(file_name: str, weight:str, sim_attr: str, ext_attr: str, threshold: float):
 
     df = read_shapefile(data_dir, file_name)
-    w = rook(df)
+    # w = Rook.from_dataframe(df)
+    if weight == 'Rook':
+        w = Rook.from_dataframe(df)
+    elif weight == 'Queen':
+        w = Queen.from_dataframe(df)
+    else:
+        print("Invalid weight selected")
     smpStartTime = time.time()
     smpLabels = scalable_maxp(df, w, sim_attr, ext_attr, threshold)
     smpEndTime = time.time()
@@ -120,13 +172,13 @@ def scalable_p(file_name: str, sim_attr: str, ext_attr: str, threshold: float):
     return smpResult
 
 
-@app.get("/api/enpoint/compareMaxP")
-def compareMaxP(file_name: str, sim_attr: str, ext_attr: str,
+@app.get("/api/endpoint/compareMaxP")
+def compareMaxP(file_name: str, weight: str, sim_attr: str, ext_attr: str,
                 threshold: float):
     
-    lmpResult = libraryMaxP(file_name, sim_attr, ext_attr, threshold)
+    lmpResult = libraryMaxP(file_name, weight, sim_attr, ext_attr, threshold)
     lmpTime = lmpResult["execution_time"]
-    smpResult = scalable_p(file_name, sim_attr, ext_attr, threshold)
+    smpResult = smp(file_name, weight, sim_attr, ext_attr, threshold)
     smpTime = smpResult["execution_time"]
     speedup = lmpTime/smpTime
 
